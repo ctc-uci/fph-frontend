a6b175fa60f896196ffb8b1f79cf39648e20591e
Madhu22 <madhuxsharma@gmail.com>
2024-01-10 18:46:14 -0800

    Setup BackendContext + Axios

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { BackendProvider } from './contexts/BackendContext.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BackendProvider>
+      <App />
+    </BackendProvider>
   </React.StrictMode>,
 );
