b9afe7e3121c2ad3ae05570be99a6fa24b1f7024
Shawn Liu <xiangjl4@uci.edu>
2024-01-15 14:57:54 -0800

    App.jsx dependencies configurated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
+import BusinessTable from './BusinessTable.jsx';
+
 
 const App = () => {
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <div>
+      <BusinessTable></BusinessTable>
+    </div>
+    
   );
 };
 
