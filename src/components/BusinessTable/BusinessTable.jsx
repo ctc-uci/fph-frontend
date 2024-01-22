4cd489e602c9d1316139d873088cf94193e5f9ac
joshualipton <jalipton11@gmail.com>
2024-01-22 14:36:25 -0800

    remove app css

--- src/components/BusinessTable/BusinessTable.jsx
+++ src/components/BusinessTable/BusinessTable.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { useEffect, useState } from 'react';
 import { useBackend } from '../../contexts/BackendContext';
 import { Table, Thead, Tbody, Tr, Td } from '@chakra-ui/react';
