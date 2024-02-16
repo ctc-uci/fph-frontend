fa7b663a3d18c84a1d48e1ad8f2370f4b7e3f6c5
Kaitlin Leung <leungkn@uci.edu>
2024-02-16 14:57:26 -0800

    Created AdminFilterBusinesses component to add tabs and buttons for filtration and pagination to AdminDashboard

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -2,15 +2,15 @@ import './AdminDashboard.module.css';
 
 import { useBackend } from '../../contexts/BackendContext';
 import { useEffect, useState } from 'react';
-import { Box, Button, IconButton, Container } from '@chakra-ui/react';
-import { ChevronRightIcon, ChevronLeftIcon } from '@chakra-ui/icons';
+import { Box } from '@chakra-ui/react';
+//import { ChevronRightIcon, ChevronLeftIcon } from '@chakra-ui/icons';
+import AdminFilterBusinesses from '../AdminFilterBusinesses/AdminFilterBusinesses.jsx';
 
 const AdminDashboard = () => {
   // Created the use states
   const { backend } = useBackend();
   const [donationData, setDonationData] = useState([]);
   const [businessDictionary, setBusinessDictionary] = useState([]);
-  const [totalSites, setTotalSites] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
@@ -18,9 +18,6 @@ const AdminDashboard = () => {
         // fetches the business table to be used in pending &total #
         const businessResponse = await backend.get('/business');
         setBusinessDictionary(businessResponse.data);
-        // fetches total amount of businesses
-        const totalSitesResponse = await backend.get('/business/totalSites');
-        setTotalSites(totalSitesResponse.data[0]["count"]);
         // fetches donation table data
         const donationResponse = await backend.get('/donation/');
         setDonationData(donationResponse.data);
@@ -52,7 +49,6 @@ const AdminDashboard = () => {
   const calculatePendingBusinesses = () => {
     var pendingBusinesses = 0;
     for (const [key, value] of Object.entries(businessDictionary)) {
-      console.log(key);
       const pendingStatus = value['status'];
       if (pendingStatus == 'Pending') {
         pendingBusinesses += 1;
@@ -67,19 +63,14 @@ const AdminDashboard = () => {
 
       <div>
         <>
-           <Box>{totalSites} Current donation sites</Box>
+           <Box>{calculateTotalDonationSites()} Current donation sites</Box>
           <Box>{calculateTotalDonationForms()} donation forms submitted</Box>
           <Box>
-            {totalSites - calculateTotalDonationForms()} donation forms not
+            {calculateTotalDonationSites() - calculateTotalDonationForms()} donation forms not
             submitted
           </Box>
           <Box>{calculatePendingBusinesses()} applications pending </Box>
-          <Container>
-            <Button>Submitted</Button>
-            <Button>Not Submitted</Button>
-          </Container>
-          <IconButton aria-label="Back button" icon={<ChevronLeftIcon/>}/>
-          <IconButton aria-label="Next button" icon={<ChevronRightIcon/>}/>
+          <AdminFilterBusinesses/>
         </>
       </div>
     </div>
---
+++ src/components/AdminFilterBusinesses/AdminFilterBusinesses.jsx
@@ -0,0 +1,67 @@
+import './AdminFilterBusinesses.module.css';
+import BusinessTable from '../BusinessTable/BusinessTable.jsx';
+
+import { useBackend } from '../../contexts/BackendContext';
+import { useEffect, useState } from 'react';
+import { Box, IconButton, Tabs, TabList, Tab } from '@chakra-ui/react';
+import { ChevronRightIcon, ChevronLeftIcon } from '@chakra-ui/icons';
+
+const AdminFilterBusinesses = () => {
+  // Created the use states
+  const { backend } = useBackend();
+  //const [donationData, setDonationData] = useState([]);
+  const [businessDictionary, setBusinessDictionary] = useState([]);
+  const [currentBusinessNum, setCurrentBusinessNum] = useState(0);
+  const [currentPageNum, setCurrentPageNum] = useState(1);
+  const [currentTab, setCurrentTab] = useState('Active'); //change to all once all page is implemented
+  const [pageLimit, setPageLimit] = useState(1);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        loadInfo(currentTab);
+        console.log(businessDictionary);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    getData();
+  }, [currentTab, currentPageNum]);
+
+  const changeTab = async (tab) => {
+    setCurrentTab(tab);
+    setCurrentPageNum(1);
+    //loadInfo(tab);
+  };
+
+  const loadInfo = async (tab) => {
+    // const businessResponse = await backend.get(`/business/?businessLimit=10&pageNum=${currentPageNum}&tab=${tab}`);
+    // setBusinessDictionary(businessResponse.data);
+    changePage();
+    const businessNumResponse = await backend.get(`/business/totalBusinesses/?tab=${tab}`);
+    setCurrentBusinessNum(businessNumResponse.data[0]["count"])
+    setPageLimit(Math.ceil(businessNumResponse.data[0]["count"] / 10));
+  };
+
+  const changePage = async () => {
+    const businessResponse = await backend.get(`/business/?businessLimit=10&pageNum=${currentPageNum}&tab=${currentTab}`);
+    setBusinessDictionary(businessResponse.data);
+  };
+
+  return (
+    <div>
+      <Tabs>
+        <TabList>
+          <Tab onClick={() => {changeTab('Active')}}>Submitted</Tab>
+          <Tab onClick={() => {changeTab('Pending')}}>Not Submitted</Tab>
+        </TabList>
+      </Tabs>
+      <BusinessTable businessData={businessDictionary}/>
+      <Box>{(currentPageNum - 1) * 10 + 1} to {Math.min(currentPageNum * 10, currentBusinessNum)} of {currentBusinessNum}</Box>
+      <IconButton aria-label='Back button' isDisabled={currentPageNum <= 1} icon={<ChevronLeftIcon/>} onClick={() => setCurrentPageNum(currentPageNum - 1)}/>
+      <IconButton aria-label='Next button' isDisabled={currentPageNum >= pageLimit} icon={<ChevronRightIcon/>} onClick={() => setCurrentPageNum(currentPageNum + 1)}/>
+    </div>
+  );
+};
+
+export default AdminFilterBusinesses;
