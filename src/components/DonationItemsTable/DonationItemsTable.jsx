d68802fb34aa3805321b6df54052389d9e62193a
Sean Kelman <seanrkelman@gmail.com>
2024-02-17 17:27:27 -0800

    replaced links with tabs to make it look more like design

--- src/components/DonationItemsTable/DonationItemsTable.jsx
+++ src/components/DonationItemsTable/DonationItemsTable.jsx
@@ -1,17 +1,54 @@
 import { useEffect, useState } from 'react';
 import { useBackend } from '../../contexts/BackendContext';
-import { Link, Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+} from '@chakra-ui/react';
 import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
+import PropTypes from 'prop-types';
 
 const DonationItemsTable = () => {
-  const { backend } = useBackend();
-  const [data, setData] = useState([]);
-  const TABLE_HEADERS = ['Category', 'Id', 'Name', 'Quantity Type', 'Price'];
+  return (
+    <div>
+      <Tabs>
+        <TabList>
+          <Tab>All</Tab>
+          <Tab>Food</Tab>
+          <Tab>Misc.</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <DonationItems category="all" />
+          </TabPanel>
+          <TabPanel>
+            <DonationItems category="Food" />
+          </TabPanel>
+          <TabPanel>
+            <DonationItems category="Misc." />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </div>
+  );
+};
 
+const DonationItems = ({ category }) => {
+  const [data, setData] = useState([]);
+  const { backend } = useBackend();
+  const TABLE_HEADERS = ['Category', 'Id', 'Name', 'Quantity Type', 'Price'];
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await backend.get('/value/filter/all');
+        const response = await backend.get(`/value/filter/${category}`);
         setData(response.data);
       } catch (error) {
         console.error('Error getting donation items:', error);
@@ -19,55 +56,37 @@ const DonationItemsTable = () => {
       }
     };
     getData();
-  }, [backend]);
-
-  const handleCategoryChange = category => {
-    return async () => {
-      try {
-        const response = await backend.get(`/value/filter/${category}`);
-        setData(response.data);
-      } catch (error) {
-        console.error('Error getting donation items:', error);
-      }
-    };
-  };
+  }, [backend, category]);
 
   return (
-    <div>
-      <Link colorScheme="blue" onClick={handleCategoryChange('all')}>
-        All
-      </Link>
-      <Link colorScheme="blue" onClick={handleCategoryChange('Food')}>
-        Food
-      </Link>
-      <Link colorScheme="blue" onClick={handleCategoryChange('Misc.')}>
-        Misc.
-      </Link>
-      <Table variant="striped">
-        <Thead>
-          <Tr>
-            {TABLE_HEADERS.map((header, index) => (
-              <Th key={index}>{header}</Th>
-            ))}
-          </Tr>
-        </Thead>
-        <Tbody>
-          {data.map((item, index) => (
-            <Tr key={index}>
-              {Object.keys(item).map(key => (
-                <Td key={key}>
-                  {typeof item[key] === 'boolean' ? (item[key] ? 'True' : 'False') : item[key]}
-                </Td>
-              ))}
-              <Td>
-                <EditIcon /> <DeleteIcon color="red" />
-              </Td>
-            </Tr>
+    <Table variant="striped">
+      <Thead>
+        <Tr>
+          {TABLE_HEADERS.map((header, index) => (
+            <Th key={index}>{header}</Th>
           ))}
-        </Tbody>
-      </Table>
-    </div>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {data.map((item, index) => (
+          <Tr key={index}>
+            {Object.keys(item).map(key => (
+              <Td key={key}>
+                {typeof item[key] === 'boolean' ? (item[key] ? 'True' : 'False') : item[key]}
+              </Td>
+            ))}
+            <Td>
+              <EditIcon /> <DeleteIcon color="red" />
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
   );
+};
+
+DonationItems.propTypes = {
+  category: PropTypes.string.isRequired,
 };
 
 export default DonationItemsTable;
