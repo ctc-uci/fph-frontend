21ab5b4f1906ba3de5d98116dfbcc74699966cc3
Kaitlin Leung <leungkn@uci.edu>
2024-04-28 00:42:47 -0700

    started account form in admin settings

--- src/components/AdminSettings/AdminAccount.jsx
+++ src/components/AdminSettings/AdminAccount.jsx
@@ -1,5 +1,47 @@
+// import { useEffect, useState } from 'react';
+// import { useBackend } from '../../contexts/BackendContext';
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Card,
+  Input,
+} from '@chakra-ui/react';
+import classes from './AdminSettings.module.css';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
+
 const AdminAccount = () => {
-  return <div>This is the AdminAccount page</div>;
+  return (
+    <>
+      <Card className={classes.roundedTable}>
+        <FormControl>
+          <Flex>
+            <FormLabel>Name</FormLabel>
+            <Input></Input>
+            <Input></Input>
+          </Flex>
+
+          <Flex>
+            <FormLabel>Email</FormLabel>
+            <Input></Input>
+          </Flex>
+
+          <Flex>
+            <FormLabel>Password</FormLabel>
+            <Button sx={{ backgroundColor: "transparent"}}>Change Password
+              <ArrowForwardIcon />
+            </Button>
+          </Flex>
+        </FormControl>
+      </Card>
+
+      <Flex>
+        <Button>Undo Changes</Button>
+        <Button>Save</Button>
+      </Flex>
+    </>
+  );
 };
 
 export default AdminAccount;
