1affa8a83562e0052f96ba1eafac817bfc175b33
Kevin Wu <kevinwu098@gmail.com>
2024-08-04 04:03:19 -0700

    feat: clean donation items table

--- src/components/DonationItemsTable/DonationItemsTable.tsx
+++ src/components/DonationItemsTable/DonationItemsTable.tsx
@@ -3,11 +3,10 @@ import { AddIcon, ChevronLeftIcon, ChevronRightIcon, DeleteIcon, EditIcon } from
 import {
   Box,
   Button,
+  Flex,
   Heading,
   HStack,
   IconButton,
-  Image,
-  Stack,
   Tab,
   Table,
   TableContainer,
@@ -17,7 +16,6 @@ import {
   Tabs,
   Tbody,
   Td,
-  Text,
   Th,
   Thead,
   Tr,
@@ -25,12 +23,10 @@ import {
 } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 
-import FemalePettingDog from '../../assets/FemalePettingDog.png';
-import MalePettingDog from '../../assets/MalePettingDog.png';
 import { useAuth } from '../../contexts/AuthContext.jsx';
 import { useBackend } from '../../contexts/BackendContext';
+import { pageStyle, pageTitleStyle } from '../../styles/sharedStyles';
 import NotificationsDrawer from '../NotificationsDrawer/NotificationsDrawer.jsx';
-import classes from './DonationItemsTable.module.css';
 import DonationsDeleteConfirmationModal from './DonationsDeleteConfirmationModal.jsx';
 import DonationsModal from './DonationsModal.jsx';
 
@@ -57,19 +53,19 @@ export const DonationItemsTable = () => {
   }, [backend]);
 
   return (
-    <div className={classes.container}>
-      <HStack sx={{ width: 'full', justifyContent: 'space-between', paddingY: 4 }}>
-        <Heading>Donation Items</Heading>
+    <Flex sx={pageStyle}>
+      <HStack sx={{ width: 'full', justifyContent: 'space-between' }}>
+        <Heading sx={pageTitleStyle}>Donation Items</Heading>
         <NotificationsDrawer notificationsData={notifications} />
       </HStack>
-      <Tabs marginTop="24px" isFitted={true} colorScheme="teal">
-        <div>
-          <TabList w="185px" mb={-10}>
-            <Tab>All</Tab>
-            <Tab>Food</Tab>
-            <Tab>Misc.</Tab>
-          </TabList>
-        </div>
+
+      <Tabs isFitted={true} colorScheme="teal">
+        <TabList width={'fit-content'} mb={-10}>
+          <Tab>All</Tab>
+          <Tab>Food</Tab>
+          <Tab>Misc.</Tab>
+        </TabList>
+
         <TabPanels>
           <TabPanel padding="0">
             <DonationItems category="all" />
@@ -82,7 +78,7 @@ export const DonationItemsTable = () => {
           </TabPanel>
         </TabPanels>
       </Tabs>
-    </div>
+    </Flex>
   );
 };
 
@@ -112,19 +108,18 @@ const DonationItems = ({ category }: { category: string }) => {
   }, [currentPageNum, category]);
 
   const loadInfo = async () => {
-    // Loads pages
     try {
       const formResponse = await backend.get(
         `/value/paginate?itemsLimit=10&pageNum=${currentPageNum}&category=${category}`,
       );
       setData(formResponse.data);
+
+      const itemsNumResponse = await backend.get(`/value/totalValues?category=${category}`);
+      setCurrentItemNum(itemsNumResponse.data[0]['count']);
+      setPageLimit(Math.ceil(itemsNumResponse.data[0]['count'] / 10));
     } catch (error) {
       console.log(error.message);
     }
-    console.log(category);
-    const itemsNumResponse = await backend.get(`/value/totalValues?category=${category}`);
-    setCurrentItemNum(itemsNumResponse.data[0]['count']);
-    setPageLimit(Math.ceil(itemsNumResponse.data[0]['count'] / 10));
   };
 
   const openDeleteModal = async (item) => {
@@ -136,42 +131,21 @@ const DonationItems = ({ category }: { category: string }) => {
     setIsEdit(isEdit);
   };
 
-  // const  deleteModalOnOpen();
-  //   await backend.delete(`/value/${item['item_id']}`);
-  //   //setDeleteModalVisible(true);
-  //   loadInfo();
-  // };
-
   return (
-    <Box>
-      <DonationsDeleteConfirmationModal
-        isOpen={deleteModalIsOpen}
-        onClose={deleteModalOnClose}
-        loadInfo={loadInfo}
-        selectedItem={selectedItem}
-      />
-      <DonationsModal
-        isOpen={donationsModalIsOpen}
-        onClose={donationsModalOnClose}
-        data={selectedItem}
-        setCurrentPageNum={setCurrentPageNum}
-        loadInfo={loadInfo}
-        isEdit={isEdit}
-      />
-      <Box className={classes.addItemContainer} mr={4}>
-        <Button
-          colorScheme="teal"
-          onClick={() => {
-            donationsModalOnOpen();
-            setEditModal(false);
-          }}
-          gap={2}
-        >
-          <AddIcon boxSize={3} />
-          Add Item
-        </Button>
-      </Box>
-      <TableContainer className={classes.roundedTable}>
+    <Flex sx={{ flexDirection: 'column', gap: 4 }}>
+      <Button
+        colorScheme="teal"
+        onClick={() => {
+          donationsModalOnOpen();
+          setEditModal(false);
+        }}
+        sx={{ marginLeft: 'auto', marginRight: 4, gap: 2, width: 'fit-content' }}
+      >
+        <AddIcon boxSize={3} />
+        Add Item
+      </Button>
+
+      <TableContainer sx={{ backgroundColor: 'white', borderRadius: 'lg', borderWidth: 1 }}>
         <Table style={{ borderCollapse: 'collapse' }}>
           <Thead>
             <Tr>
@@ -181,7 +155,7 @@ const DonationItems = ({ category }: { category: string }) => {
             </Tr>
           </Thead>
           <Tbody>
-            {data.map((item, index) => (
+            {data?.map((item, index) => (
               <Tr key={index}>
                 {Object.keys(item).map((key) => (
                   <Td key={key}>
@@ -213,7 +187,8 @@ const DonationItems = ({ category }: { category: string }) => {
           </Tbody>
         </Table>
       </TableContainer>
-      <div className={classes.resultNavigation}>
+
+      <HStack sx={{ justifyContent: 'flex-end' }}>
         <Box>
           {(currentPageNum - 1) * 10 + 1} to {Math.min(currentPageNum * 10, currentItemNum)} of{' '}
           {currentItemNum}
@@ -230,25 +205,22 @@ const DonationItems = ({ category }: { category: string }) => {
           icon={<ChevronRightIcon />}
           onClick={() => setCurrentPageNum(currentPageNum + 1)}
         />
-      </div>
-      <Box position="absolute" left="40%">
-        {currentPageNum === pageLimit && category === 'Food' && (
-          <>
-            <Image src={MalePettingDog} alt="Male Image" />
-            <Text ml="10" color="blackAlpha.400">
-              You’ve reached the end!
-            </Text>
-          </>
-        )}
-        {currentPageNum === pageLimit && category !== 'Food' && (
-          <>
-            <Image src={FemalePettingDog} alt="Female Image" />
-            <Text ml="4" color="blackAlpha.400">
-              You’ve reached the end!
-            </Text>
-          </>
-        )}
-      </Box>
-    </Box>
+      </HStack>
+
+      <DonationsDeleteConfirmationModal
+        isOpen={deleteModalIsOpen}
+        onClose={deleteModalOnClose}
+        loadInfo={loadInfo}
+        selectedItem={selectedItem}
+      />
+      <DonationsModal
+        isOpen={donationsModalIsOpen}
+        onClose={donationsModalOnClose}
+        data={selectedItem}
+        setCurrentPageNum={setCurrentPageNum}
+        loadInfo={loadInfo}
+        isEdit={isEdit}
+      />
+    </Flex>
   );
 };
